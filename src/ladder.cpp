72e6346da299a90432d27185388e943bf2c53290
WaynelleOfficial <scwaynelle@gmail.com>
2025-03-16 17:52:02 +0000

    still fizing formating newline

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -131,6 +131,7 @@ void print_word_ladder(const vector<string>& ladder){
     for (size_t i = 0; i < ladder.size(); i++){
         cout << ladder[i] << " ";
     }
+    cout << "\n";
 }
 
 #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
